cdab867d2f819e23edf5859ca613e9a6e618f3ff
Mateus Marques <mateusmarques2001@usp.br>
2025-02-25 21:55:28 -0300

    Included phpactor lang server

--- init.lua
+++ init.lua
@@ -668,6 +668,7 @@ require('lazy').setup({
         bashls = {},
         html = {},
         cssls = {},
+        phpactor = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
